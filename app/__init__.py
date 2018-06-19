42a216ac45dfbd861a60c8c479801477e98a0a01
Elvis Amuni <elvisamuni22@gmail.com>
2018-06-19 12:43:38 +0300

    This commit will import a class to the init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
 #nitializing application
 app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
 
 from app import views
