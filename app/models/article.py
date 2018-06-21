b90dceac1d84cb4651d4a53c5b15a78ab0a30e4d
Elvis Amuni <elvisamuni22@gmail.com>
2018-06-21 12:55:51 +0300

    This commit will create a class called article

--- app/models/article.py
+++ app/models/article.py
@@ -3,7 +3,11 @@ class Article:
     A class that defines our articles object
     '''
 
-    def __init__(self, id ,name,title,author, description, url, datePublished):
+    def __init__(self, id ,name, title, author, description, url, datePublished):
         self.id = id
         self.name = name
-        
+        self.title = title
+        self.author = author
+        self.description = description
+        self.url = url
+        self.datePublished = datePublished
