815bdd1ccc388dab9f7d6b171c74d9979a96c8cc
Elvis Amuni <elvisamuni22@gmail.com>
2018-06-21 12:53:13 +0300

    This commit will create a new .py file

---
+++ app/models/article.py
@@ -0,0 +1,9 @@
+class Article:
+    '''
+    A class that defines our articles object
+    '''
+
+    def __init__(self, id ,name,title,author, description, url, datePublished):
+        self.id = id
+        self.name = name
+        
