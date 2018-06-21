f3711929f13aaaf8065d36568a4a8632ce8f8054
Elvis Amuni <elvisamuni22@gmail.com>
2018-06-21 11:12:51 +0300

    This commit will modify the views file

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -7,4 +8,18 @@ def index():
     '''
     Function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    general_list = get_news('general')
+    health_list = get_news('health')
+    business_list = get_news('business')
+    technology_list = get_news('technology')
+    sports_list = get_news('sports')
+    entertainment_list = get_news('entertainment')
+
+    return render_template('index.html',
+                            general=general_list,
+                            health=health_list,
+                            business=business_list,
+                            technology=technology_list,
+                            sports=sports_list,
+                            entertainment=entertainment_list)
